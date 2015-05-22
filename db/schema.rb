1204dbd32cff5bd9df30dab98c9bf996dd6fb955
maximx <maxim7667@outlook.com>
2015-05-22 13:27:31 +0800

    add sub-category model and update db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522045106) do
+ActiveRecord::Schema.define(version: 20150522050405) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20150522045106) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "user_id",     limit: 4
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.integer  "category_id", limit: 4
+    t.integer  "serial",      limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,25 @@ user.save
 
 
 # init category
-category_name_lists = [ "3C物品", "遊戲", "車", "書籍、影片", "服飾裝扮", "空間倉庫", "作業工具", "運動器具" ]
-category_name_lists.each_with_index do |name, index|
-  category = Category.new(name: name, serial: index)
+category_list = [ "3C物品", "遊戲", "車", "書籍、影片", "服飾裝扮", "空間倉庫", "作業工具", "其他" ]
+category_list.each_with_index do |name, serial|
+  category = Category.new(name: name, serial: serial)
   category.save
+end
+
+subcategory_list = [
+  [ "攝影用具", "電腦及周邊", "其他家電" ],
+  [ "遊戲主機", "遊戲光碟", "桌上遊戲" ],
+  [ "休旅車", "小客車", "貨車" ],
+  [ "戲劇影集", "漫畫", "小說", "其他書籍" ],
+  [ "禮服", "首飾配件", "cosplay" ],
+  [ "活動場地", "倉庫", "格子" ],
+  [ "五金工具", "大型機臺" ],
+  [ "運動用具", "旅行物品", "其他" ]
+]
+subcategory_list.each_with_index do |sub_list, category_id|
+  sub_list.each_with_index do |sub_name, serial|
+    subcategory = Subcategory.new(name: sub_name, serial: serial, category_id: category_id)
+    subcategory.save
+  end
 end
