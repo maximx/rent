26e3f103e7658b466219fc32ebb9814f61446cfc
maximx <maxim7667@outlook.com>
2015-05-22 12:29:37 +0800

    beautify items/show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :render, class_name: :User, foreign_key: :user_id
+
+  enum period: [ :時, :日, :月, :年 ]
+
+  def editable_by?(user)
+    user && user == render
+  end
 end
