368d107db2552254c55fc71659336a1f4e657496
maximx <maxim7667@outlook.com>
2015-05-22 13:02:42 +0800

    add category model and db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522023840) do
+ActiveRecord::Schema.define(version: 20150522045106) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "serial",     limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "name",        limit: 255
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# init user
 user = User.new(email: "maxim7667@outlook.com", password: "julius23")
 user.save
+
+
+# init category
+category_name_lists = [ "3C物品", "遊戲", "車", "書籍、影片", "服飾裝扮", "空間倉庫", "作業工具", "運動器具" ]
+category_name_lists.each_with_index do |name, index|
+  category = Category.new(name: name, serial: index)
+  category.save
+end
