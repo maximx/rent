8b5f6f90c1d6c7953800336505197b5420c4b71b
maximx <maxim7667@outlook.com>
2015-05-24 17:59:34 +0800

    refactor items show page

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+  def render_collect_link(item)
+    if current_user && current_user.is_collected?(item)
+       collect_link = link_to("取消收藏", uncollect_item_path(item), method: :delete)
+    else
+       collect_link = link_to("收藏", collect_item_path(item), method: :post)
+    end
+
+    return collect_link
+  end
 end
