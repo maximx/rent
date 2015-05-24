9c4e2942bcdc32d02a88f33e36fe1ff87ff2d812
maximx <maxim7667@outlook.com>
2015-05-24 18:18:01 +0800

    refactor users show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def render_follow_link(user)
+    if current_user && current_user.is_following?(user)
+      follow_link = link_to("取消追蹤", unfollow_user_path(user), method: :delete)
+    else
+      follow_link = link_to("追蹤", follow_user_path(user), method: :post)
+    end
+  end
 end
