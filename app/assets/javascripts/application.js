3923c78ae90939f802ebe53f8cc8efcb62657452
maximx <maxim7667@outlook.com>
2015-05-25 10:08:47 +0800

    ad bootstrap javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
 //= require underscore
 //= require gmaps/google
 //= require_tree .
