00e6bdd156bbbc272b8e316f2fbf9b6ac6365fec
maximx <maxim7667@outlook.com>
2015-05-29 20:13:04 +0800

    beautify alert and notice message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,35 @@ module ApplicationHelper
       render partial: "layouts/category"
     end
   end
+
+  def render_alert(msg)
+    @msg = msg
+    alert_notice_tag("danger") if msg.present?
+  end
+
+  def render_notice(msg)
+    @msg = msg
+    alert_notice_tag("warning") if msg.present?
+  end
+
+  private
+
+  def alert_notice_tag(type)
+    content_tag(:p, notice_content_with_close,
+      class: "alert alert-#{type} alert-dismissible",
+      role: :alert
+    )
+  end
+
+  def notice_content_with_close
+    content_tag(:button, span_close,
+      type: :button, class: :close,
+      data: { dismiss: :alert }, aria: { label: :Close }
+    ) +
+    content_tag(:strong, @msg)
+  end
+
+  def span_close
+    content_tag(:span, "x", aria: { hidden: true })
+  end
 end
