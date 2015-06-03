8e1ce5f163866acb5265686edd3dc7a70004a4e6
maximx <maxim7667@outlook.com>
2015-06-03 14:24:43 +0800

    add fullcalendar for item rent records

--- app/models/rent_record.rb
+++ app/models/rent_record.rb
@@ -1,4 +1,14 @@
 class RentRecord < ActiveRecord::Base
   belongs_to :borrower, class_name: "User", foreign_key: "user_id"
   belongs_to :item
+
+  def as_json(options={})
+    {
+      id: id,
+      title: "#{name} - #{borrower.email}",
+      start: started_at,
+      end: ended_at,
+      url: Rails.application.routes.url_helpers.item_rent_record_path(item, id)
+    }
+  end
 end
