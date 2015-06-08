0315c2c9b1ec7912d53ec94062f82b81fd220847
maximx <maxim7667@outlook.com>
2015-06-08 11:36:23 +0800

    indent rent_cloudinary methods

--- app/controllers/concerns/rent_cloudinary.rb
+++ app/controllers/concerns/rent_cloudinary.rb
@@ -1,23 +1,25 @@
 module RentCloudinary
 
-  def set_public_id
-    if params[model_name].has_key?(:pictures_attributes)
-      pictures_attributes["0"][:public_id].each_with_index do |picture, index|
-        pictures_attributes["#{index}"] = { public_id: upload_to_cloudinary(picture) }
+  private
+
+    def set_public_id
+      if params[model_name].has_key?(:pictures_attributes)
+        pictures_attributes["0"][:public_id].each_with_index do |picture, index|
+          pictures_attributes["#{index}"] = { public_id: upload_to_cloudinary(picture, index) }
+        end
       end
     end
-  end
 
-  def upload_to_cloudinary(pic)
-    Cloudinary::Uploader.upload(pic)["public_id"]
-  end
+    def upload_to_cloudinary(pic, index)
+      Cloudinary::Uploader.upload(pic, use_filename: true)["public_id"]
+    end
 
-  def pictures_attributes
-    params[model_name][:pictures_attributes]
-  end
+    def pictures_attributes
+      params[model_name][:pictures_attributes]
+    end
 
-  def model_name
-    params[:controller].singularize
-  end
+    def model_name
+      params[:controller].singularize
+    end
 
 end
