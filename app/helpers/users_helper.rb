7753754fc7778e11c3ea987f9f3a1661fbb3f5c9
maximx <maxim7667@outlook.com>
2015-06-08 23:22:42 +0800

    beautify items show and users show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def render_follow_link(user)
     if current_user && current_user.is_following?(user)
-      follow_link = link_to("取消追蹤", unfollow_user_path(user), method: :delete)
+      link_to("取消追蹤", unfollow_user_path(user), method: :delete, class: "btn btn-default")
     else
-      follow_link = link_to("追蹤", follow_user_path(user), method: :post)
+      link_to("追蹤", follow_user_path(user), method: :post, class: "btn btn-primary")
     end
   end
 end
