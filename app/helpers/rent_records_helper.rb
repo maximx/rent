322313c6d263d5dada567e62867ce3e3d1d61a90
maximx <maxim7667@outlook.com>
2015-06-10 10:53:44 +0800

    modify method wording

--- app/helpers/rent_records_helper.rb
+++ app/helpers/rent_records_helper.rb
@@ -1,12 +1,14 @@
 module RentRecordsHelper
 
-  def render_edit_item_rent_record_link(rent_record)
+  def render_edit_rent_record_link(rent_record)
     if rent_record.editable_by?(current_user)
-      link_to("修改", edit_item_rent_record_path(rent_record.item, rent_record), class: "btn btn-info")
+      link_to("修改",
+              edit_item_rent_record_path(rent_record.item, rent_record),
+              class: "btn btn-default")
     end
   end
 
-  def render_renting_item_rent_record_link(rent_record)
+  def render_renting_rent_record_link(rent_record)
     if rent_record.can_rent_by?(current_user)
       link_to("確認出租",
               renting_item_rent_record_path(rent_record.item, rent_record),
@@ -16,7 +18,7 @@ module RentRecordsHelper
     end
   end
 
-  def render_returning_item_rent_record_link(rent_record)
+  def render_returning_rent_record_link(rent_record)
     if rent_record.can_return_by?(current_user)
       link_to("確認歸還",
               returning_item_rent_record_path(rent_record.item, rent_record),
@@ -26,19 +28,21 @@ module RentRecordsHelper
     end
   end
 
-  def render_review_item_rent_record_link(rent_record)
+  def render_review_rent_record_link(rent_record)
     if rent_record.can_review_by?(current_user)
-      link_to("評價", review_item_rent_record_path(rent_record.item, rent_record), class: "btn btn-info")
+      link_to("評價",
+              review_item_rent_record_path(rent_record.item, rent_record),
+              class: "btn btn-info")
     end
   end
 
-  def render_show_item_rent_record_link(rent_record)
+  def render_show_rent_record_link(rent_record)
     if rent_record.viewable_by?(current_user)
       link_to("查閱", item_rent_record_path(rent_record.item, rent_record), class: "btn btn-default")
     end
   end
 
-  def render_ask_for_review_item_rent_record_link(rent_record)
+  def render_ask_for_review_rent_record_link(rent_record)
     if rent_record.can_ask_review_by?(current_user)
       link_to("邀請評價",
               ask_for_review_item_rent_record_path(rent_record.item, rent_record),
