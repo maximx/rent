77e299501bd58c6393b50f4a4421aa1db823b0de
maximx <maxim7667@outlook.com>
2015-06-10 10:31:49 +0800

    refactor item helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,11 +1,9 @@
 module ItemsHelper
-  def render_collect_link(item)
+  def render_collect_item_link(item)
     if current_user && current_user.is_collected?(item)
-       collect_link = link_to("取消收藏", uncollect_item_path(item), method: :delete, class: "btn btn-default")
+       link_to("取消收藏", uncollect_item_path(item), method: :delete, class: "btn btn-default")
     else
-       collect_link = link_to("收藏", collect_item_path(item), method: :post, class: "btn btn-info")
+       link_to("收藏", collect_item_path(item), method: :post, class: "btn btn-info")
     end
-
-    return collect_link
   end
 end
