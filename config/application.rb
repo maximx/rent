d2c1605c1ebbdbd00ef14cac74fe67232f0c9b29
maximx <maxim7667@outlook.com>
2015-06-15 10:06:48 +0800

    add devise i18n zh-TW

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Rent
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.i18n.available_locales = [:"zh-TW"]
+    config.i18n.default_locale = :"zh-TW"
   end
 end
