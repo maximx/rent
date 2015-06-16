c9d73ff8335448a91f4e9c6412020a69d9ff836b
maximx <maxim7667@outlook.com>
2015-06-16 19:24:36 +0800

    beautify settings/items index page

--- app/helpers/settings/items_helper.rb
+++ app/helpers/settings/items_helper.rb
@@ -1,2 +1,11 @@
 module Settings::ItemsHelper
+
+  def render_renting_checkbox(count)
+    if count == 1
+      content_tag(:span, "", class: "glyphicon glyphicon-ok")
+    else
+      content_tag(:span, "", class: "glyphicon glyphicon-unchecked")
+    end
+  end
+
 end
