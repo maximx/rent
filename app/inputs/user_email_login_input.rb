73ec3eb6d8d91d5a52af3aa07d5d0f0f5b2e6858
maximx <maxim7667@outlook.com>
2015-06-23 11:54:40 +0800

    add i18n for user model attributes

--- app/inputs/user_email_login_input.rb
+++ app/inputs/user_email_login_input.rb
@@ -2,7 +2,7 @@ class UserEmailLoginInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
     input_wrapper_options = merge_wrapper_options(input_html_options, wrapper_options)
     template.content_tag(:div, class: "col-sm-10") do
-      @builder.text_field(attribute_name, input_wrapper_options)
+      @builder.email_field(attribute_name, input_wrapper_options)
     end
   end
 end
