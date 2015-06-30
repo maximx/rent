0601539e4c163ef9aaac577f3d6ee580ddf43831
maximx <maxim7667@outlook.com>
2015-06-30 15:03:45 +0800

    change gmail apps password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'maxim7667@gmail.com',
-    password: 'noifxsmdrmhuvckr',
+    password: 'tbssvzmnjoqcsmhh',
     authentication: 'plain',
     enable_starttls_auto: true
   }
