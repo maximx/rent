359f121f34cf0fbcf7e95673969f14bfcb8d4b29
maximx <maxim7667@outlook.com>
2015-07-14 20:15:53 +0800

    visual design of settings/rent_records page

--- app/helpers/rent_records_helper.rb
+++ app/helpers/rent_records_helper.rb
@@ -2,63 +2,64 @@ module RentRecordsHelper
 
   def render_edit_rent_record_link(rent_record)
     if rent_record.editable_by?(current_user)
-      link_to("修改",
+      link_to(render_icon("edit", class: "text-success"),
               edit_item_rent_record_path(rent_record.item, rent_record),
-              class: "btn btn-default")
+              class: "btn btn-default", title: "修改", data: { toggle: "tooltip" })
     end
   end
 
   def render_renting_rent_record_link(rent_record)
     if rent_record.can_rent_by?(current_user)
-      link_to("確認出租",
+      link_to(render_icon("ok", class: "text-primary"),
               renting_item_rent_record_path(rent_record.item, rent_record),
               method: :put,
-              class: "btn btn-primary",
-              data: { confirm: "確認已交付出租物嗎？" })
+              class: "btn btn-default", title: "確認出租",
+              data: { toggle: "tooltip", confirm: "確認已交付出租物嗎？" })
     end
   end
 
   def render_returning_rent_record_link(rent_record)
     if rent_record.can_return_by?(current_user)
-      link_to("確認歸還",
+      link_to(render_icon("home", class: "text-warning"),
               returning_item_rent_record_path(rent_record.item, rent_record),
               method: :put,
-              class: "btn btn-danger",
-              data: { confirm: "確認已歸還出租物嗎？" })
+              class: "btn btn-default", title: "確認歸還",
+              data: { toggle: "tooltip", confirm: "確認已歸還出租物嗎？" })
     end
   end
 
   def render_withdrawing_rent_record_link(rent_record)
     if rent_record.can_withdraw_by?(current_user)
-      link_to("取消預訂",
+      link_to(render_icon("remove", class: "text-danger").html_safe,
               withdrawing_item_rent_record_path(rent_record.item, rent_record),
               method: :delete,
-              class: "btn btn-danger",
-              data: { confirm: "確定要取消預約#{rent_record.item.name}嗎？"})
+              class: "btn btn-default", title: "取消預訂",
+              data: { toggle: "tooltip", confirm: "確定要取消預約#{rent_record.item.name}嗎？"})
     end
   end
 
   def render_review_rent_record_link(rent_record)
     if rent_record.can_review_by?(current_user)
-      link_to("評價",
+      link_to(render_icon("thumbs-up", class: "text-info"),
               review_item_rent_record_path(rent_record.item, rent_record),
-              class: "btn btn-info")
+              class: "btn btn-default", title: "評價", data: { toggle: "tooltip" })
     end
   end
 
   def render_show_rent_record_link(rent_record)
     if rent_record.viewable_by?(current_user)
-      link_to("查閱", item_rent_record_path(rent_record.item, rent_record), class: "btn btn-default")
+      link_to(render_icon("zoom-in", class: "text-info"), item_rent_record_path(rent_record.item, rent_record),
+              class: "btn btn-default", title: "查閱", data: { toggle: "tooltip" })
     end
   end
 
   def render_ask_for_review_rent_record_link(rent_record)
     if rent_record.can_ask_review_by?(current_user)
-      link_to("邀請評價",
+      link_to(render_icon("send", class: "text-success"),
               ask_for_review_item_rent_record_path(rent_record.item, rent_record),
               method: :post,
-              class: "btn btn-success",
-              data: { confirm: "將寄出信件邀請對方評價，確認要繼續嗎？"})
+              class: "btn btn-default", title: "邀請評價",
+              data: { toggle: "tooltip", confirm: "將寄出信件邀請對方評價，確認要繼續嗎？"})
     end
   end
 
