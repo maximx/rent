eaea955461fc9ea411cb894471efb2b03f882956
maximx <maxim7667@outlook.com>
2015-07-16 15:49:11 +0800

    visual design of items/rent_recors/show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,14 @@
 module UsersHelper
-  def render_follow_link(user)
+  def render_follow_link(user, type = "")
     if current_user && current_user.is_following?(user)
-      link_to("取消追蹤", unfollow_user_path(user), method: :delete, class: "btn btn-default")
+      render_unfollow_link(user, type)
     else
       link_to("追蹤", follow_user_path(user), method: :post, class: "btn btn-primary")
     end
+  end
+
+  def render_unfollow_link(user, type = "")
+    type = (type.to_sym == :remote)
+    link_to("取消追蹤", unfollow_user_path(user), method: :delete, class: "btn btn-default unfollow-user", remote: type)
   end
 end
