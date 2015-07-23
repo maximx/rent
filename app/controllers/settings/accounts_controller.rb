4a16a9cd721005c3826fd7eecd5b8296cc3fff60
maximx <maxim7667@outlook.com>
2015-07-23 12:09:12 +0800

    add flash notcie messag to settings/accounts update action

--- app/controllers/settings/accounts_controller.rb
+++ app/controllers/settings/accounts_controller.rb
@@ -14,9 +14,18 @@ class Settings::AccountsController < ApplicationController
 
   def update
     @user = User.find(current_user.id)
+    prev_unconfirmed_email = @user.unconfirmed_email
+
     if @user.update_with_password(user_params)
+      flash_key = if update_needs_confirmation?(@user, prev_unconfirmed_email)
+                    :update_needs_confirmation
+                  else
+                    :updated
+                  end
+      flash[:notice] = I18n.t("devise.registrations.#{flash_key}")
+
       sign_in @user, bypass: true
-      redirect_to settings_account_path
+      redirect_to edit_settings_account_path
     else
       render :edit
     end
@@ -24,10 +33,14 @@ class Settings::AccountsController < ApplicationController
 
   private
 
+    def update_needs_confirmation?(user, prev_email)
+      prev_email != user.unconfirmed_email
+    end
+
     def user_params
       params.require(:user).permit(
         :password, :password_confirmation,
-        :email, :current_password
+        :email, :current_password, :account
       )
     end
 
