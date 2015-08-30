0cbebff7ce16f4b791a579b360ecfd703f41f8b0
maximx <maxim7667@outlook.com>
2015-08-30 15:15:32 +0800

    add bank_account validator on profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,6 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_one :picture, as: :imageable, dependent: :destroy
   validates_length_of :bank_code, minimum: 3, maximum:3 , allow_blank: true
+  validates_presence_of :bank_account, if: :bank_code?
   accepts_nested_attributes_for :picture
 end
