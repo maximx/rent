4819abf91640c5caeffa5f5085da599395ded1a3
maximx <maxim7667@outlook.com>
2015-08-31 09:34:26 +0800

    refactor i18n message on rent_record model

--- app/models/concerns/i18n_message.rb
+++ app/models/concerns/i18n_message.rb
@@ -2,8 +2,13 @@ module I18nMessage
 
   private
 
-    def i18n_message(message, options = {})
+    def i18n_activerecord_error(message, options = {})
       translation = "activerecord.errors.models.#{self.class.name.underscore}.attributes.#{message}"
       I18n.t(translation, options)
+    end
+
+    def i18n_activerecord_attribute(attr)
+      translation = "activerecord.attributes.#{self.class.name.underscore}.#{attr}"
+      I18n.t(translation)
     end
 end
