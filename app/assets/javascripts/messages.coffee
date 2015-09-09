e6b2a6e15bd597799e390ef7abb7f06ec9ea6161
maximx <maxim7667@outlook.com>
2015-09-09 11:48:11 +0800

    add error message and visual design on messages/_new page

--- app/assets/javascripts/messages.coffee
+++ app/assets/javascripts/messages.coffee
@@ -1,3 +1,7 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$ ->
+  $('#message_form').on 'ajax:success', (e, data, status, xhr) ->
+    if data && data.status == 'ok'
+      $('#message_form_success').removeClass('hide').find('code').text(data.message)
+      $('#messages_modal').modal('hide')
+    else
+      $('#message_body').closest('.form-group').find('.hint-error').html(data.message)
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,19 @@ class MessagesController < ApplicationController
   before_action :find_recipient
 
   def create
-    message = current_user.send_message(@recipient, message_params[:body], subject)
+    result = { status: 'error' }
+    if request.xhr?
+      message = current_user.send_message(@recipient, message_params[:body], subject)
+      if message.id
+        result = { status: 'ok', message: '成功送出訊息' }
+      elsif message_params[:body].blank?
+        result[:message] = '<code>請填寫訊息內容</code>'
+      end
+    end
+
+    respond_to do |format|
+      format.json { render json: result }
+    end
   end
 
   private
