0798e85692fd62f2410bb1e5913298e9ed9534d6
maximx <maxim7667@outlook.com>
2015-09-14 14:24:45 +0800

    add conversation/show action and visual design on conversation/show page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,14 +1,23 @@
 class ConversationsController < ApplicationController
   before_action :login_required
   before_action :find_mailbox
+  before_action :find_conversation, except: [ :index ]
 
   def index
     @conversations = @mailbox.inbox
+  end
+
+  def show
+    @conversation.mark_as_read(current_user)
   end
 
   private
 
     def find_mailbox
       @mailbox = current_user.mailbox
+    end
+
+    def find_conversation
+      @conversation = @mailbox.conversations.find(params[:id])
     end
 end
--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,6 @@
 module ConversationsHelper
+  def render_conversation_subject(conversation)
+    conversation.is_unread?(current_user) ?
+      content_tag(:strong, conversation.subject) : conversation.subject
+  end
 end
