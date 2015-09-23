edd69166ddad5f5519ed776dfa363b015d5420bd
maximx <maxim7667@outlook.com>
2015-09-23 19:16:53 +0800

    refactor pictures_attrs_setter

--- app/controllers/concerns/pictures_attrs_setter.rb
+++ app/controllers/concerns/pictures_attrs_setter.rb
@@ -2,14 +2,9 @@ module PicturesAttrsSetter
   private
 
     def set_picture_attrs
-      if view_context.items_controller? and picture_selected?
-        pictures_attributes["0"][:public_id].each_with_index do |picture, index|
-          pictures_attributes["#{index}"] = { public_id: picture.original_filename, file_cached: picture }
-        end
-      elsif view_context.profiles_controller?
-        if picture_attributes.has_key?(:public_id)
-          picture_attributes[:file_cached] =  picture_attributes[:public_id]
-          picture_attributes[:public_id] = picture_attributes[:file_cached].original_filename
+      if view_context.profiles_controller?
+        if params[model_name][:picture_attributes].has_key?(:public_id)
+          set_attrs( params[model_name][:picture_attributes] )
         else
           # avoid picture delete
           params[model_name].delete(:picture_attributes)
@@ -17,25 +12,28 @@ module PicturesAttrsSetter
       end
     end
 
+    def set_pictures_attrs
+      if view_context.items_controller? and picture_selected?
+        pictures_attributes["0"][:public_id].each_with_index do |picture, index|
+          pictures_attributes["#{index}"] = { public_id: picture.original_filename, file_cached: picture }
+        end
+      end
+    end
+
     def set_attachment_attrs
       if view_context.rent_records_controller? and attchment_selected?
-        attachments_attributes[:file_cached] =  attachments_attributes[:public_id]
-        attachments_attributes[:public_id] = attachments_attributes[:file_cached].original_filename
+        set_attrs( params['rent_record_state_log'][:attachments_attributes]["0"] )
       end
+    end
+
+    def set_attrs(file_attributes)
+      file_attributes[:file_cached] =  file_attributes[:public_id]
+      file_attributes[:public_id] = file_attributes[:file_cached].original_filename
     end
 
     # item
     def pictures_attributes
       params[model_name][:pictures_attributes]
-    end
-
-    # user profile
-    def picture_attributes
-      params[model_name][:picture_attributes]
-    end
-
-    def attachments_attributes
-      params['rent_record_state_log'][:attachments_attributes]["0"]
     end
 
     def model_name
