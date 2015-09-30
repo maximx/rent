1b4ee384dc374e840587705fb64f5c5ae0e40be8
maximx <maxim7667@outlook.com>
2015-09-30 20:09:03 +0800

    fix log modal error on dashboar/records index page and fix validify_date method exception error

--- app/controllers/dashboard/records_controller.rb
+++ app/controllers/dashboard/records_controller.rb
@@ -4,6 +4,11 @@ class Dashboard::RecordsController < ApplicationController
 
   def index
     @rent_records = @item.rent_records.actived.rencent.page(params[:page])
+    @rent_record_state_log = unless @rent_records.empty?
+                               @rent_records.first.rent_record_state_logs.build
+                             else
+                               RentRecordStateLog.new
+                             end
   end
 
   private
