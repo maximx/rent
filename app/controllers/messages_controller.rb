101429555fa32a9498f518f561f6e67ce3c4af30
maximx <maxim7667@outlook.com>
2015-10-06 10:12:16 +0800

    refacotr message subject and body in messages/create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,14 @@
 class MessagesController < ApplicationController
   before_action :login_required
-  before_action :find_recipient
   before_action :find_item
 
   def create
     result = { status: 'error', message: '來源錯誤' } unless request.xhr?
-    result = { status: 'error', message: '錯誤的收訊人' } if current_user == @recipient
+    result = { status: 'error', message: '錯誤的收訊人' } if current_user == @item.lender
     result = { status: 'error', message: '請填寫訊息內容' } if message_params[:body].blank?
 
     unless result
-      message = current_user.send_message(@recipient, message_body, subject)
+      message = current_user.send_message(@item.lender, message_body, subject)
       if message.id
         result = { status: 'ok', message: '成功送出訊息' }
       else
@@ -25,11 +24,7 @@ class MessagesController < ApplicationController
   private
 
     def message_params
-      params.require(:message).permit(:recipient, :item_id, :body)
-    end
-
-    def find_recipient
-      @recipient = User.find(message_params[:recipient])
+      params.require(:message).permit(:item_id, :body)
     end
 
     def find_item
