9cfdfc366370ca3b42a84411b6ea86cf059cfa15
maximx <maxim7667@outlook.com>
2015-10-12 14:39:18 +0800

    visual design on logo and favicon

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,6 +1,6 @@
 module NotificationsHelper
   def render_notifications_link
-    icon = render_icon('bell', title: '通知與訊息', data: { toggle: 'tooltip', placement: 'bottom' })
+    icon = render_icon('comment', title: '通知與訊息', data: { toggle: 'tooltip', placement: 'bottom' })
     badge = content_tag(:span, current_user.unread_count, class: 'badge')
 
     link_to(icon + badge, notifications_path, class: 'conversations-icon')
