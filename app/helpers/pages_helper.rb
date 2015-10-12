a5f4ae9507486959d5265ffa3d8b6a583d114b61
maximx <maxim7667@outlook.com>
2015-10-12 14:54:21 +0800

    visual design on layouts root

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+  def render_root_navbar_class
+    default_class = pages_index_action? ? '' : ' navbar-default'
+    "navbar navbar-fixed-top" + default_class
+  end
+
+  def pages_index_action?
+    params[:controller] == 'pages' and params[:action] == 'index'
+  end
 end
