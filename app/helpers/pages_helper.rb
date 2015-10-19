e7dde2a7af6ea28b7a97b2f4b0bf5df2e2a11f28
maximx <maxim7667@outlook.com>
2015-10-19 13:50:07 +0800

    visual desigon navbar on common/header

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,4 @@
 module PagesHelper
-  def render_root_navbar_class
-    default_class = pages_index_action? ? '' : ' navbar-fixed-top'
-    "navbar navbar-default " + default_class
-  end
-
   def pages_index_action?
     params[:controller] == 'pages' and params[:action] == 'index'
   end
