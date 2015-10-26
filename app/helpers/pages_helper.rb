a4ef57ab3dbeadeb588d53768db3938f4b994bbd
maximx <maxim7667@outlook.com>
2015-10-26 11:00:42 +0800

    visual design footer navbar pages/footer

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,8 @@
 module PagesHelper
   def pages_navbar_list
-    render_link_li class: 'nav navbar-nav' do |li|
+    render_link_li class: 'nav navbar-nav footer-links' do |li|
       li << [ '關於', about_path ]
-      li << [ '使用條款', terms_path ]
+      li << [ '服務條款', terms_path ]
       li << [ '隱私權政策', privacy_path ]
     end
   end
