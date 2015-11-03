5fa9910dbfc3caaf107a13107b091de9c859681c
maximx <maxim7667@outlook.com>
2015-11-03 10:22:26 +0800

    update meta keywords

--- config/initializers/rent.rb
+++ config/initializers/rent.rb
@@ -1,6 +1,6 @@
 module Rent
   DEFAULT_AVATAR = 'default-avatar'
-  SITE_NAME = '廣和行'
-  KEYWORDS = [ SITE_NAME, '出租', '承租', '租', '共享經濟' ]
+  SITE_NAME = '廣和租賃行'
+  KEYWORDS = [ SITE_NAME, SITE_NAME[0..1], '出租網', '共享經濟', '租經濟', '個人出租', '租東西', '借賃' ]
   DESCRIPTION = "在特定時日需要的某些東西，而不想花大錢購買？#{SITE_NAME}上有著各家出租業者、個人所提供的物品，以使用者付費觀念承租所需物品，亦可出租家中閒置物品。"
 end
