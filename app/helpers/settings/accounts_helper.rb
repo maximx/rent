9a57499a77648560cee516ded249604da1306d80
maximx <maxim7667@outlook.com>
2015-11-14 09:40:49 +0800

    add images_settings_account_path to settings account navbar list

--- app/helpers/settings/accounts_helper.rb
+++ app/helpers/settings/accounts_helper.rb
@@ -2,6 +2,7 @@ module Settings::AccountsHelper
   def settings_navbar_list
     render_link_li class: 'nav navbar-nav' do |li|
       li << [ render_icon_with_text('user', '基本資料'), settings_account_path ]
+      li << [ render_icon_with_text('picture', '頭像與封面'), images_settings_account_path ]
       li << [ render_icon_with_text('lock', '帳戶設定'), edit_settings_account_path ]
     end
   end
