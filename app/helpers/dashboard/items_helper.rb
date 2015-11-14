42be033bb1677c8fa4509e8856e547ff326d72e7
maximx <maxim7667@outlook.com>
2015-11-15 04:04:27 +0800

    add dashboard_customers_path to dashboard_navbar_list

--- app/helpers/dashboard/items_helper.rb
+++ app/helpers/dashboard/items_helper.rb
@@ -5,10 +5,11 @@ module Dashboard::ItemsHelper
 
   def dashboard_navbar_list
     render_link_li class: 'nav navbar-nav' do |li|
-      li << [ render_icon_with_text("list", "出租物管理"), dashboard_items_path ]
-      li << [ render_icon_with_text("record", "承租紀錄"), dashboard_rent_records_path ]
-      li << [ render_icon_with_text("calendar", "出租日曆"), calendar_dashboard_rent_records_path(role: "lender") ]
-      li << [ render_icon_with_text("calendar", "承租日曆"), calendar_dashboard_rent_records_path(role: "borrower") ]
+      li << [ render_icon_with_text('list', '出租物管理'), dashboard_items_path ]
+      li << [ render_icon_with_text('user', '客戶管理'), dashboard_customers_path ]
+      li << [ render_icon_with_text('record', '承租紀錄'), dashboard_rent_records_path ]
+      li << [ render_icon_with_text('calendar', '出租日曆'), calendar_dashboard_rent_records_path(role: 'lender') ]
+      li << [ render_icon_with_text('calendar', '承租日曆'), calendar_dashboard_rent_records_path(role: 'borrower') ]
     end
   end
 
