e49f37df871b1ee37d7bc01b1e0e78547902485f
maximx <maxim7667@outlook.com>
2015-11-14 12:43:14 +0800

    update category and subcategory

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,7 @@ user.save
 
 
 # init category
-# 車可能會有麻煩，先移除
-#category_list = [ "3C物品", "遊戲", "書籍、影片", "服飾裝扮", "空間倉庫", "作業工具", "其他", "車" ]
-category_list = [ "3C物品", "遊戲", "書籍、影片", "服飾裝扮", "作業工具", "其他" ]
+category_list = [ "3C物品", "遊戲相關", "書籍、影片", "服飾", "其他" ]
 category_list.each_with_index do |name, serial|
   category = Category.new(name: name, serial: serial)
   category.save
@@ -30,11 +28,8 @@ subcategory_list = [
   [ "攝影用具", "電腦及周邊", "其他家電" ],
   [ "遊戲主機", "遊戲光碟", "桌上遊戲" ],
   [ "戲劇影集", "漫畫", "小說", "其他書籍" ],
-  [ "禮服", "首飾配件", "Cosplay" ],
-  #[ "活動場地", "倉庫", "格子" ],
-  [ "五金工具", "大型機臺" ],
+  [ "禮服", "Cosplay", "其他服飾" ],
   [ "運動用具", "旅行物品", "其他" ]
-  #[ "休旅車", "小客車", "貨車" ]
 ]
 subcategory_list.each_with_index do |sub_list, category_id|
   sub_list.each_with_index do |sub_name, serial|
