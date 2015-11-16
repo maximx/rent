a990cbe6235a21e2336600eea2902b977002853d
maximx <maxim7667@outlook.com>
2015-11-16 10:54:48 +0800

    add dashboard/rent_records/create action and update related page

--- app/controllers/dashboard/rent_records_controller.rb
+++ app/controllers/dashboard/rent_records_controller.rb
@@ -1,6 +1,6 @@
 class Dashboard::RentRecordsController < ApplicationController
   before_action :login_required
-  before_action :find_item, only: [ :index, :new ]
+  before_action :find_item, only: [ :index, :new, :create ]
 
   def index
     @rent_records = if find_item?
@@ -25,6 +25,21 @@ class Dashboard::RentRecordsController < ApplicationController
     render 'rent_records/new'
   end
 
+  def create
+    @rent_record = @item.rent_records.build rent_record_params
+    @rent_record.deliver = Deliver.face_to_face
+
+    customer = current_user.customers.find @rent_record.borrower_id
+    @rent_record.borrower = customer
+
+    if @rent_record.save
+      redirect_to item_rent_record_path(@item, @rent_record)
+    else
+      flash[:alert] = '請檢查紅字錯誤欄位'
+      render :new
+    end
+  end
+
   def calendar
     @event_sources_path = calendar_dashboard_rent_records_path(format: :json, role: params[:role])
     rent_records_json = if params[:role] == "borrower"
@@ -42,6 +57,10 @@ class Dashboard::RentRecordsController < ApplicationController
   end
 
   private
+
+    def rent_record_params
+      params.require(:rent_record).permit(:borrower_id, :borrower_type, :started_at, :ended_at)
+    end
 
     def find_item
       @item = current_user.items.find params[:item_id] if find_item?
--- app/helpers/dashboard/customers_helper.rb
+++ app/helpers/dashboard/customers_helper.rb
@@ -1,25 +1,31 @@
 module Dashboard::CustomersHelper
-  def render_show_customer_link(customer)
-    link = customer.is_a?(Customer) ? dashboard_customer_path(customer) : user_path(customer)
-
+  def render_show_borrower_link(borrower)
     link_to render_icon('zoom-in'),
-            link,
+            borrower_path(borrower),
             class: 'btn btn-default', title: '查閱', data: { toggle: 'tooltip' }
   end
 
-  def render_edit_customer_link(customer)
-    if customer.is_a? Customer
+  def render_edit_customer_link(borrower)
+    if is_customer? borrower
       link_to render_icon('edit', class: 'text-success'),
-              edit_dashboard_customer_path(customer),
+              edit_dashboard_customer_path(borrower),
               class: 'btn btn-default', title: '修改', data: { toggle: 'tooltip' }
     end
   end
 
   def render_operate_customer_links(customer)
     links = raw [
-      render_show_customer_link(customer),
+      render_show_borrower_link(customer),
       render_edit_customer_link(customer)
     ].join
     content_tag :div, links, class: 'btn-group'
+  end
+
+  def borrower_path borrower
+    is_customer?(borrower) ? dashboard_customer_path(borrower) : user_path(borrower)
+  end
+
+  def is_customer? borrower
+    borrower.is_a? Customer
   end
 end
