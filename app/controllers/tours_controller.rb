90ef04759e65f83da022fdc8c0c4227f04f62585
maximx <maxim7667@outlook.com>
2015-12-01 11:57:29 +0800

    add meta title to controller tours

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,4 +1,6 @@
 class ToursController < ApplicationController
+  before_action :set_title, :set_pages_meta_tags
+
   def index
   end
 
@@ -13,4 +15,17 @@ class ToursController < ApplicationController
 
   def dashboard
   end
+
+  private
+
+    def set_title
+      @title = "#{t('controller.name.tours')} - #{t("controller.tours.#{action_name}")}"
+    end
+
+    def set_pages_meta_tags
+      set_meta_tags(
+        title: @title,
+        og: { title: @title }
+      )
+    end
 end
