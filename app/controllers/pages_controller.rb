7418bd1d4d2ae8d02e9871ae9170a40094b342c3
maximx <maxim7667@outlook.com>
2015-12-01 11:48:41 +0800

    add meta title on views/page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   layout 'user', only: [ :index ]
 
+  before_action :set_title, :set_pages_meta_tags, except: [ :index ]
+
   def index
   end
 
@@ -16,4 +18,18 @@ class PagesController < ApplicationController
   def contact
     @admin = User.find 1
   end
+
+
+  private
+
+    def set_title
+      @title = t("controller.action.#{action_name}")
+    end
+
+    def set_pages_meta_tags
+      set_meta_tags(
+        title: @title,
+        og: { title: @title }
+      )
+    end
 end
