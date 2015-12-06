fa1d9a692b270cc06aa26e3dd399213b0ec9448a
maximx <maxim7667@outlook.com>
2015-12-06 10:14:08 +0800

    add model attachment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204085541) do
+ActiveRecord::Schema.define(version: 20151206021325) do
+
+  create_table "attachments", force: :cascade do |t|
+    t.integer  "attachable_id",   limit: 4
+    t.string   "attachable_type", limit: 255
+    t.string   "file",            limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "attachments", ["attachable_type", "attachable_id"], name: "index_attachments_on_attachable_type_and_attachable_id", using: :btree
 
   create_table "banks", force: :cascade do |t|
     t.string   "code",       limit: 255
