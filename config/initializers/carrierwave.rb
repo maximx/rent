8186b756434e6823e6655d7eee53dc20c22500d2
maximx <maxim7667@outlook.com>
2015-12-07 10:54:06 +0800

    add config/fog.yml for security

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
+FOG_CONFIG = YAML.load_file(Rails.root.join('config', 'fog.yml'))[Rails.env]
+
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAILD7RSL6EKZH56EA',
-    aws_secret_access_key: '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',
+    aws_access_key_id: FOG_CONFIG['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: FOG_CONFIG['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-southeast-1'
   }
   config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.fog_directory = Rails.env.production? ? 'guangho-file' : 'guangho-test'
+  config.fog_directory = FOG_CONFIG['AWS_BUCKET']
 end
