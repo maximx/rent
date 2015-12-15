2db5041397b61eb3b2a09b77616e39daf6dd007a
maximx <maxim7667@outlook.com>
2015-12-15 09:43:58 +0800

    remove unused db migration

--- db/migrate/20151105030619_add_format_to_pictures.rb
+++
@@ -1,5 +0,0 @@
-class AddFormatToPictures < ActiveRecord::Migration
-  def change
-    add_column :pictures, :format, :string
-  end
-end
--- db/migrate/20151208120620_drop_table_questions.rb
+++
@@ -1,5 +0,0 @@
-class DropTableQuestions < ActiveRecord::Migration
-  def change
-    drop_table :questions
-  end
-end
