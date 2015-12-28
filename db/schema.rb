6f52022c5ecfd8d8dd8e74e1c4dd684e9b92c509
maximx <maxim7667@outlook.com>
2015-12-28 10:04:02 +0800

    Add model order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227124128) do
+ActiveRecord::Schema.define(version: 20151228020301) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer  "attachable_id",     limit: 4
@@ -174,6 +174,15 @@ ActiveRecord::Schema.define(version: 20151227124128) do
 
   add_index "mailboxer_receipts", ["notification_id"], name: "index_mailboxer_receipts_on_notification_id", using: :btree
   add_index "mailboxer_receipts", ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type", using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "user_type",  limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "orders", ["user_type", "user_id"], name: "index_orders_on_user_type_and_user_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id",               limit: 4
