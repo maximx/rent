14d67445f675dc7ddf2752447e8746745ab2b96c
maximx <maxim7667@outlook.com>
2015-12-30 08:50:40 +0800

    Add account/orders tablist

--- app/helpers/account/records_helper.rb
+++ app/helpers/account/records_helper.rb
@@ -12,11 +12,4 @@ module Account::RecordsHelper
       breadcrumb :new_account_item_record, item
     end
   end
-
-  def render_account_records_index_calendar
-    render_link_li class: 'nav nav-tabs', role: 'tablist' do |li|
-      li << [ render_icon_with_text('list-alt', t('controller.account/records.action.index')), account_records_path ]
-      li << [ render_icon_with_text('calendar', t('controller.action.calendar')), calendar_account_records_path ]
-    end
-  end
 end
