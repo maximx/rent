f5ef99b4248c7ad78274c7d50d3235ba26a8d3b7
maximx <maxim7667@outlook.com>
2016-01-15 16:09:30 +0800

    Improve ux on model concern currency_price

--- app/models/concerns/currency_price.rb
+++ app/models/concerns/currency_price.rb
@@ -2,7 +2,15 @@ module CurrencyPrice
   extend ActiveSupport::Concern
 
   ['price', 'deposit', 'total_price', 'deliver_fee'].each do |attr|
-    define_method("currency_#{attr}") { "$#{send(attr)}" }
-    define_method("currency_item_#{attr}") { "$#{send("item_#{attr}")}" } # for model records
+    define_method("currency_#{attr}") do
+      value = send(attr)
+      (value == value.to_i) ?  "$#{value.to_i}" : "$#{value}"
+    end
+
+    # for model records
+    define_method("currency_item_#{attr}") do
+      value = send("item_#{attr}")
+      (value == value.to_i) ?  "$#{value.to_i}" : "$#{value}"
+    end
   end
 end
